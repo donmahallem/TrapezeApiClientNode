eccbdce8a4b46784b7808b536750019aee2d92a5
DonMahallem <donmahallem@users.noreply.github.com>
2019-05-05 13:04:39 +0200

    init

--- src/vehicle.storage.ts
+++ src/vehicle.storage.ts
@@ -12,69 +12,85 @@ export enum Status {
 
 export interface IBaseStatus {
     status: Status;
+    timestamp?: number;
 }
 
-export interface IErrorStatus {
+export interface IErrorStatus extends IBaseStatus {
     status: Status.ERROR;
     error: any;
 }
 
-export interface ISuccessStatus {
+export interface ISuccessStatus extends IBaseStatus {
     status: Status.SUCCESS;
     storage: Map<string, IVehicleLocation>;
     tripStorage: Map<string, IVehicleLocation>;
+    lastUpdate: number;
 }
 
 export type LoadStatus = ISuccessStatus | IErrorStatus;
 
 export class VehicleStorage {
 
-    private lastUpdate: number = -1;
     private lock: LockHandler = new LockHandler(false);
-    private currentStatus: LoadStatus;
-    constructor(private trapezeClient: TrapezeApiClient, private updateDelay: number = 10000) {
+    private mStatus: LoadStatus;
+    constructor(private trapezeClient: TrapezeApiClient, private updateDelay: number = 10000) { }
 
+    public updateRequired(): boolean {
+        if (this.status) {
+            return this.status.timestamp + this.updateDelay < Date.now();
+        }
+        return true;
+    }
+
+    public get status(): LoadStatus {
+        return this.mStatus;
     }
 
     public fetch(): Promise<LoadStatus> {
-        if (this.lastUpdate + this.updateDelay > Date.now()) {
-            return Promise.resolve(this.currentStatus);
+        if (this.updateRequired()) {
+            return Promise.resolve(this.status);
         }
         if (this.lock.locked) {
-            return this.lock.promise().then(() => this.currentStatus);
+            return this.lock.promise().then(() => this.status);
         }
         this.lock.locked = true;
         return this.trapezeClient.getVehicleLocations()
             .then((result: IVehicleLocationList): ISuccessStatus => {
-                const loadStatus: ISuccessStatus = {
-                    status: Status.SUCCESS,
-                    storage: new Map(),
-                    tripStorage: new Map(),
-                };
-                for (const entry of result.vehicles) {
-                    if (entry === null) {
-                        continue;
-                    }
-                    if (entry.isDeleted === true) {
-                        continue;
-                    }
-                    const vehicleLocation: IVehicleLocation = entry as IVehicleLocation;
-                    loadStatus.storage.set(vehicleLocation.id, vehicleLocation);
-                    loadStatus.tripStorage.set(vehicleLocation.tripId, vehicleLocation);
-                }
-                this.lastUpdate = Date.now();
-                this.currentStatus = loadStatus;
-                this.lock.locked = false;
-                return loadStatus;
+                return this.convertResponse(result);
             }, (err: any): IErrorStatus => {
                 const errorStatus: IErrorStatus = {
                     error: err,
                     status: Status.ERROR,
                 };
-                this.currentStatus = errorStatus;
-                this.lock.locked = false;
                 return errorStatus;
+            })
+            .then((loadStatus: LoadStatus): LoadStatus => {
+                loadStatus.timestamp = Date.now();
+                this.mStatus = loadStatus;
+                this.lock.locked = false;
+                return loadStatus;
             });
+    }
+
+    public convertResponse(result: IVehicleLocationList): ISuccessStatus {
+        const loadStatus: ISuccessStatus = {
+            status: Status.SUCCESS,
+            storage: new Map(),
+            tripStorage: new Map(),
+            lastUpdate: result.lastUpdate,
+        };
+        for (const entry of result.vehicles) {
+            if (entry === null) {
+                continue;
+            }
+            if (entry.isDeleted === true) {
+                continue;
+            }
+            const vehicleLocation: IVehicleLocation = entry as IVehicleLocation;
+            loadStatus.storage.set(vehicleLocation.id, vehicleLocation);
+            loadStatus.tripStorage.set(vehicleLocation.tripId, vehicleLocation);
+        }
+        return loadStatus;
     }
 
     public getVehicleByTripId(id: string): Promise<IVehicleLocation> {
@@ -104,11 +120,21 @@ export class VehicleStorage {
             });
     }
 
-    public getVehicles(left: number, right: number, top: number, bottom: number): Promise<IVehicleLocation[]> {
+    /**
+     * @since 2.0.0
+     * @param left 
+     * @param right 
+     * @param top 
+     * @param bottom 
+     */
+    public getVehicles(left: number, right: number, top: number, bottom: number): Promise<IVehicleLocationList> {
         return this.fetch()
-            .then((status: LoadStatus) => {
+            .then((status: LoadStatus): IVehicleLocationList => {
                 if (status.status === Status.SUCCESS) {
-                    const vehicleList: IVehicleLocation[] = new Array();
+                    const vehicleList: IVehicleLocationList = {
+                        vehicles: new Array(),
+                        lastUpdate: status.lastUpdate,
+                    };
                     for (const key of Array.from(status.storage.keys())) {
                         const vehicle: IVehicleLocation = status.storage.get(key);
                         if (vehicle.longitude < left || vehicle.longitude > right) {
@@ -116,7 +142,7 @@ export class VehicleStorage {
                         } else if (vehicle.latitude > top || vehicle.latitude < bottom) {
                             continue;
                         } else {
-                            vehicleList.push(vehicle);
+                            vehicleList.vehicles.push(vehicle);
                         }
                     }
                     return vehicleList;
