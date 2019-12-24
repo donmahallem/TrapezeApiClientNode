a7cfbcc589fb849818b56ba1bb0df2a8007436da
donmahallem <donmahallem@users.noreply.github.com>
2019-12-24 22:42:22 +0100

    fix

--- src/vehicle-dataset.spec.ts
+++ src/vehicle-dataset.spec.ts
@@ -2,6 +2,7 @@
  * Source https://github.com/donmahallem/TrapezeApiClientNode
  */
 
+import { IVehicleLocationList } from "@donmahallem/trapeze-api-types";
 import { expect } from "chai";
 import * as Loki from "lokijs";
 import "mocha";
@@ -49,6 +50,47 @@ describe("vehicle-dataset.ts", () => {
         after(() => {
             sandbox.restore();
             clock.restore();
+        });
+        describe("convertToDatabaseEntries(result)", () => {
+            const testData: IVehicleLocationList = {
+                lastUpdate: 235236,
+                vehicles: [
+                    undefined,
+                    // tslint:disable-next-line:no-null-keyword
+                    null,
+                    {
+                        isDeleted: true,
+                    },
+                    {
+                        id: "testId1",
+                        latitude: 1,
+                        tripId: "tripId1",
+                    } as any,
+                    {
+                        id: "testId2",
+                        longitude: 2,
+                        tripId: "tripId2",
+                    } as any,
+                    {
+                        id: "testId3",
+                        latitude: 3,
+                        longitude: 4,
+                        tripId: "tripId3",
+                    } as any,
+                ],
+            };
+            it("should parse the items correctly", () => {
+                const result: any[] = instance.convertToDatabaseEntries(testData);
+                expect(result.length).to.equal(1);
+                expect(result.every((value) => value.lastUpdate === 235236)).to.equal(true);
+                expect(result).to.deep.equal([{
+                    id: "testId3",
+                    lastUpdate: 235236,
+                    latitude: 3,
+                    longitude: 4,
+                    tripId: "tripId3",
+                }]);
+            });
         });
         describe("addLocationResponse(vehicles)", () => {
             let convertStub: sinon.SinonStub;
